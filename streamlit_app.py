87b1e71606d826e8a40e83192df7e2f72159b268
ZigorSalvador <159166872+ZigorSalvador@users.noreply.github.com>
2025-03-31 11:10:39 +0000

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("AI Tester")
+
+with st.container(border=True):
+
+    prompt = st.selectbox(
+    "Versión",
+    ("v1.0.0 (20250331)", "v1.1.0 (20250403)", "v1.2.0 (20250410)", "prueba Aleix", "prueba Zigor"),
+    )
+
+    optinGlosario = st.checkbox("Glosario terminológico")
+
+    optinTDB = st.checkbox("Technical Database")
+
+with st.container(border=True):
+    
+    prompt = st.chat_input("Consulta técnica...")
+
+    st.button("Enviar", key=1)
+
+with st.container(border=True):
+
+    st.text("Aquí se inyectaría la respuesta a la pregunta...")
+    st.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+with st.container(border=True):
+
+    st.write("Feedback")
+    st.feedback("thumbs")
+
+    prompt = st.chat_input("Comentarios...")
+    
+    st.button("Enviar", key=2)
